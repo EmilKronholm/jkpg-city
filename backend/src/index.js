df596894ca396f86917c56e0526cccecf905673b
ekkecode <emil.kronholm@gmail.com>
2025-02-18 19:48:42 +0100

    Sample get request to test databse

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,10 +1,21 @@
 const express = require('express')
+const pool = require("./db");
 
 const app = express()
 
 app.get('/', (req, res) => {
     res.status(200).send("Docker is easy 🐬")
 })
+
+app.get("/person", async (req, res) => {
+    try {
+        const result = await pool.query("SELECT * FROM person");
+        res.json(result.rows);
+    } catch (err) {
+        console.error(err.message);
+        res.status(500).send("Server Error");
+    }
+});
 
 const port = process.env.port || 8080;
 
