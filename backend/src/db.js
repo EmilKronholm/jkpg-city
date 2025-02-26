49334bd3c10f3770c4e595b3abcb13f46c95e63a
Emil Kronholm <emil.kronholm@gmail.com>
2025-02-26 03:45:10 +0100

    Log current user and password
    To verify correct .env setup

--- backend/src/db.js
+++ backend/src/db.js
@@ -5,8 +5,10 @@ const pool = new Pool({
     user: process.env.DB_USER,
     host: process.env.DB_HOST,
     database: process.env.DB_NAME,
-    password: process.env.DB_PASSWORD,
     port: process.env.DB_PORT,
+    password: process.env.DB_PASSWORD
 });
+
+console.log(process.env.DB_USER, process.env.DB_PASSWORD)
 
 module.exports = pool;
