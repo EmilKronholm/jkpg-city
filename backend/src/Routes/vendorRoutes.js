872a57b188a20273963c969e380c1f9949d53da2
Emil Kronholm <emil.kronholm@gmail.com>
2025-03-02 12:25:10 +0100

    Update vendorRoutes.js

--- backend/src/Routes/vendorRoutes.js
+++ backend/src/Routes/vendorRoutes.js
@@ -9,12 +9,17 @@ router.get('/vendors', async (req, res) => {
 });
 
 // Get vendor based on ID
-router.get('/vendors/:id',async (req, res) => {
+router.get('/vendors/:id', async (req, res) => {
     const vendorJSON = await VendorServices.getVendor();
     res.status(200).json(vendorJSON);
 });
 
 // Update vendor. ID via query and new field passed via body (json)
 router.put('/vendors/:id', async (req, res) => {
+    const { id, url, name, districtFK } = req.body;
+    const result = await VendorServices.updateVendor(id, url, name, districtFK);
+    res.status(200).json(result);
 
 });
+
+module.exports = router
