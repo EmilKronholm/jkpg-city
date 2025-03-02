2162b49616648d93f842abeebe991debaaa74084
Emil Kronholm <emil.kronholm@gmail.com>
2025-03-02 12:25:06 +0100

    Import vendors from json to sql

--- backend/src/playground.js
+++ backend/src/playground.js
@@ -1,5 +1,12 @@
 require("dotenv").config();
 const { Pool } = require("pg");
+const fs = require('fs');
+const { rawListeners } = require("process");
+const { json } = require("stream/consumers");
+
+const data = fs.readFileSync('./src/data.json')
+const jsonData = JSON.parse(data);
+
 
 const pool = new Pool({
     user: process.env.DB_USER,
@@ -10,16 +17,38 @@ const pool = new Pool({
 });
 
 const uniqueTags = [
-	'Öster',
-	null,
-	'Väster',
-	'Tändsticksområdet',
-	'Atollen',
-	'Resecentrum'
+    'Tändsticksområdet',
+    'Atollen',
+    'Väster',
+    'Öster',
+    'Resecentrum'
 ]
 
-uniqueTags.forEach((x) => {
-    if (!x) return;
-	console.log(`${x}`)
-    pool.query(`INSERT INTO district (create_time, name) VALUES (NOW(), $1)`, [x]);
-});
+const createTags = () => {
+    uniqueTags.forEach((x) => {
+        if (!x) return;
+        console.log(`${x}`)
+        pool.query(`INSERT INTO district (create_time, name) VALUES (NOW(), $1)`, [x]);
+    });
+};
+
+const createVendors = () => {
+    // console.log(jsonData);
+    jsonData.forEach(async (vendor) => {
+        // console.log("\n \n Nu kör vi:")
+        // console.log(vendor)
+        const name = vendor.name
+        const url = vendor.url
+        let districtFK = uniqueTags.indexOf(vendor.district) + 1;
+        districtFK = (districtFK == 0) ? null : districtFK;
+        if (districtFK == null) {
+            console.log(vendor)
+        }
+
+        // await pool.query(`INSERT INTO vendors (name, url, districtFK) values
+        // ($1, $2, $3)`, [name, url, districtFK]);
+    });
+}
+createVendors();
+
+// console.log(Object.keys(jsonData).length)
