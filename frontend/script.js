0a6443d66e5641d0f45818ee7be99e70fbbc790f
Emil Kronholm <emil.kronholm@gmail.com>
2025-03-06 22:07:41 +0100

    Front end work

--- frontend/script.js
+++ frontend/script.js
@@ -1,44 +1,68 @@
 const apiURL = "http://localhost:3000"
 
-fetch("http://localhost:3000")
-            .then(response => response.text())
-            .then(data => console.log(data))
-            .catch(error => console.error("Error:", error));
+// Test ping
+// fetch("http://localhost:3000")
+//             .then(response => response.text())
+//             .then(data => console.log(data))
+//             .catch(error => console.error("Error:", error));
 
+
+function getSearch() {
+    const urlParams = new URLSearchParams(window.location.search);
+    const param = urlParams.get('search');
+    return param || "";
+}
 
 function getVendorMockData() {
     return { "id": 1, "name": "Klock & ur", "url": "example.com", "district": "väster" };
 }
 
-const ul = document.querySelector("#all-vendors ul");
 
+const ul = document.querySelector("#all-vendors ul");
 function appendVendor(vendor) {
     const li = document.createElement("li");
-    
+
     const a = document.createElement("a");
-    a.setAttribute("href", `./vendor/${vendor.id}`);
+    a.setAttribute("href", `./vendor.html?vendorID=${vendor.id}`);
     a.textContent = vendor.name;
     li.appendChild(a);
     ul.appendChild(li);
 }
 
-appendVendor(getVendorMockData());
+// appendVendor(getVendorMockData());
 
 async function fetchVendorsFromAPI(limit = 5, offset = 0) {
-    const result = await fetch(`${apiURL}/vendors?limit=${limit}`);
+    const search = getSearch()
+    console.log("test", search)
+    const result = await fetch(`${apiURL}/vendors?limit=${limit}&search=${search}`);
     const json = await result.json();
-    return await result.json();
+    return json;
 }
 
 async function main() {
-    const vendors = await fetchVendorsFromAPI(10, 0);
-    console.log(vendors)
-    
+    const vendors = await fetchVendorsFromAPI(200, 0);
+
     for (const key in vendors) {
         let vendor = vendors[key];
-        console.log(vendor);
         appendVendor(vendor);
     }
 }
 
 main();
+
+function search() {
+
+    const searchInput = document.getElementById('search-input');
+    const searchPhrase = searchInput.value;
+    search.value = "";
+
+    const urlParams = new URLSearchParams(window.location.search);
+    urlParams.set('search', searchPhrase);
+    window.history.replaceState(null, null, "?" + urlParams.toString());
+    
+    ul.innerHTML = ""
+    const li = document.createElement('h3')
+    li.textContent = "Showing results for " + searchPhrase
+    ul.appendChild(li)
+    main()
+}
---
+++ frontend/vendor-script.js
@@ -0,0 +1,65 @@
+const apiURL = "http://localhost:3000";
+
+function getID() {
+    const urlParams = new URLSearchParams(window.location.search);
+    const param = urlParams.get('vendorID');
+    return param;
+}
+
+async function getVendorFromID(id) {
+    const result = await fetch(`${apiURL}/vendors/${id}`);
+    const json = await result.json();
+    return json;
+}
+
+async function main() {
+    // Step 1: Retrieve ID and the matching data
+    const ID = getID();
+    
+    // No ID was given, therfore we cannot show any vendor
+    if (ID == undefined) {
+        const vendorSection = document.getElementById('vendor');
+        const h2 = document.createElement('h2');
+        h2.textContent = "No store ID given in URL!";
+        vendorSection.appendChild(h2)
+        return;
+    }
+    
+    const json = await getVendorFromID(ID) ;
+    const store = json[0];
+
+    // Store with id id wasn't found
+    if (store == undefined) {
+        const vendorSection = document.getElementById('vendor');
+        const h2 = document.createElement('h2');
+        h2.textContent = "Vendor with id " + ID + " wasn't found.";
+        vendorSection.appendChild(h2)
+        return;
+    }
+
+    // Step 2: Build the dom
+    const vendorSection = document.getElementById('vendor');
+    const vendorDIV = document.createElement('div')
+    
+    // Create the Heading
+    const h2 = document.createElement("h2");
+    h2.textContent = store.name;
+
+    // Create the link
+    const a = document.createElement('a');
+    a.textContent = "Offical Website"
+    a.setAttribute("href", `https://${store.url}`)
+
+    // Create the district
+    const p = document.createElement('p');
+    p.textContent = store.district;
+    
+    // Finaliez the creation 
+    vendorDIV.appendChild(h2);
+    vendorDIV.appendChild(a);
+    vendorDIV.appendChild(p);
+
+    vendorSection.appendChild(vendorDIV);  
+}
+main();
+
