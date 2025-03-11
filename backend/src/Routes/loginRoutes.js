30fc960a818c5c4ad58d25985572a20223bafb79
Emil Kronholm <emil.kronholm@gmail.com>
2025-03-11 14:53:16 +0100

    Remove unused login route

--- backend/src/Routes/loginRoutes.js
+++ backend/src/Routes/loginRoutes.js
@@ -6,25 +6,6 @@ const router = express.Router()
 
 const JWT_SECRET = "super_secret_key";
 
-const user = {
-    user1: { password: '1234' }
-};
-
-router.post('/login', (req, res) => {
-    const { username, password } = req.body;
-
-    console.log(username, password);
-
-    if (username === "u" && password === "p") {
-        const token = jwt.sign({ username }, JWT_SECRET, { expiresIn: '1h' });
-
-        res.cookie('auth_token', token, { httpOnly: true, secure: false }); //secure: false for development, true for production:
-        return res.status(200).json({ message: 'Logged in successfully' });
-    }
-
-    res.status(401).json({ message: 'Invalid credentials' });
-});
-
 const authenticateJWT = (req, res, next) => {
     const token = req.cookies.auth_token || undefined;
 
